3a1414efa88817efc9684d7315298615d16d9416
Sagar081JH <sghumare11@gmail.com>
2024-08-05 16:26:01 +0530

    price format

--- src/main/java/com/estore/impl/UserImpl.java
+++ src/main/java/com/estore/impl/UserImpl.java
@@ -199,12 +199,35 @@ public class UserImpl implements UserService {
                     long price = cartItem.getPrice();
                     totalPrice = totalPrice + price;
                 }
-                return Optional.of(totalPrice);
+                return Optional.of(getIndianCurrencyFormat(Long.toString(totalPrice)));
             }
             return Optional.of("Empty cart!");
         }
         return Optional.of("User not found!");
     }
+
+    public String getIndianCurrencyFormat(String amount) {
+        StringBuilder stringBuilder = new StringBuilder();
+        char amountArray[] = amount.toCharArray();
+        int a = 0, b = 0;
+        for (int i = amountArray.length - 1; i >= 0; i--) {
+            if (a < 3) {
+                stringBuilder.append(amountArray[i]);
+                a++;
+            } else if (b < 2) {
+                if (b == 0) {
+                    stringBuilder.append(",");
+                    stringBuilder.append(amountArray[i]);
+                    b++;
+                } else {
+                    stringBuilder.append(amountArray[i]);
+                    b = 0;
+                }
+            }
+        }
+        return stringBuilder.reverse().toString();
+    }
+
 
     @Override
     public boolean removeCartItem(long cart_item_id) {
